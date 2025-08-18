c779cc40bb84acbc9fef9c76a979fe2b63c0665d
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-08-18 17:08:31 +0530

    fix: Correct import path casing for ApiError utility

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.models.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import Jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
