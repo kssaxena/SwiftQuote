1e24445aeeae743fb246c5bc5ffdf6776df413b2
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-04-01 16:58:50 +0530

    feat changes -conditioning updated for showing full year in date conversion string

--- frontend/src/utils/mongoDB_DateTime.js
+++ frontend/src/utils/mongoDB_DateTime.js
@@ -3,7 +3,8 @@ function formatDate(mongoDate) {
   const date = new Date(mongoDate);
   const dd = String(date.getDate()).padStart(2, "0");
   const mm = String(date.getMonth() + 1).padStart(2, "0");
-  const yy = String(date.getFullYear()).slice(-2);
+  // const yy = String(date.getFullYear()).slice(-2);
+  const yy = String(date.getFullYear());
   return `${dd}/${mm}/${yy}`;
 }
 
